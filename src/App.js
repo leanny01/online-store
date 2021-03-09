f40b607360a8122c6fa18d90d24207bb5651a7d8
leanny01 <dleannykalangu@gmail.com>
2021-03-09 05:17:12 +0200

    added style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
