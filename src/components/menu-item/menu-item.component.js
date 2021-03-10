0a2971ee43506b6e95d3371850a16dadece86356
leanny01 <dleannykalangu@gmail.com>
2021-03-10 05:48:11 +0200

    added size to blocks

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./menu-item.style.scss";
 
-const MenuItem = ({ title, imageUrl }) => (
-  <div className="menu-item" style={{ backgroundImage: `url(${imageUrl})` }}>
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div
+    className={`${size} menu-item`}
+    style={{ backgroundImage: `url(${imageUrl})` }}
+  >
+    <div
+      className="background-image"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    />
     <div className="content">
       <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
