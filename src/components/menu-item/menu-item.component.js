ac94bef1b99985fb0242ba7f7dba2866130dda54
leanny01 <dleannykalangu@gmail.com>
2021-03-10 05:27:07 +0200

    added font

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -4,7 +4,7 @@ import "./menu-item.style.scss";
 const MenuItem = ({ title, imageUrl }) => (
   <div className="menu-item" style={{ backgroundImage: `url(${imageUrl})` }}>
     <div className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
